a167747175068a281f767cee27ad442fdc9a7a71
Himanshu Singh <himanshu.singhs@outlook.in>
2018-06-10 18:28:33 +0200

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocaleService, TranslationService, Language } from 'angular-l10n';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @Language() lang: string;
   title = 'app';
+
+  constructor(public locale: LocaleService, public translation: TranslationService) { }
+
+  ngOnInit(): void {
+    this.translation.translationChanged().subscribe(
+      () => { this.title = this.translation.translate('Title'); }
+    );
+  }
+
+  selectLanguage(language: string): void {
+    this.locale.setCurrentLanguage(language);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { L10nConfig, L10nLoader, TranslationModule, StorageStrategy, ProviderType } from 'angular-l10n';
+import { HttpClientModule } from '@angular/common/http';
+
+const l10nConfig: L10nConfig = {
+  locale: {
+    languages: [
+      { code: 'en', dir: 'ltr' },
+      { code: 'de', dir: 'ltr' }
+    ],
+    language: 'en',
+    storage: StorageStrategy.Cookie
+  },
+  translation: {
+    providers: [
+      { type: ProviderType.Static, prefix: './assets/locale-' }
+    ],
+    caching: true,
+    missingValue: 'No key'
+  }
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    TranslationModule.forRoot(l10nConfig)
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public l10nLoader: L10nLoader) {
+    this.l10nLoader.load();
+  }
+}
